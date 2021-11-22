a60fc7fc3774b306e91fce654f86ffe37e14c7d9
Emily Barwinczak <ebarw@MacBook-Air.local>
2021-11-22 13:27:06 -0500

    new commit with possible routes

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,12 @@
-// Search for a country
+let express = require("express")
+const db = require("../models")
+// const router = require("./auth")
+const router = express.Router()
+const post = require("../models/post")
+const sequelize = require("sequelize")
+
+
+// SEARCH
 router.get('/search', (req, res) => {
     console.log('result route')
     console.log(req.query)
@@ -12,3 +20,30 @@ router.get('/search', (req, res) => {
       res.send('RESULTS NOT RENDERING')
     })
   })
+
+// Form to Create Post
+router.get("/new", (req, res)=>{
+    db.post.findAll()
+    .then((post) => {
+        res.render("posts/new", {post: post})
+    })
+    .catch((error) => {
+        res.status(200).send("new posts")
+    })
+})
+
+// Post to Profile
+router.post("/new", (req, res) => {
+    db.post.create({
+        city: req.body.city,
+        country: req.body.country,
+        content: req.body.content,
+        photo: req.body.photo
+    })
+    .then((post) => {
+        res.render("posts/new", {post: post})
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+})
