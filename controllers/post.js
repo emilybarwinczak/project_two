c4a9c33c24c2cb3830701967dc0445e83f5ba6b9
Emily Barwinczak <ebarw@macbook-air.home>
2021-12-08 17:47:58 -0500

    saving progress before reverting commit

--- controllers/post.js
+++ controllers/post.js
@@ -12,53 +12,53 @@ const upload = multer({ dest: './uploads/' })
 
 // SEARCH
 router.get('/search', (req, res) => {
-    console.log('result route')
-    console.log(req.query)
-    db.post.findAll({
-      where: { location: req.query.location }
-    }).then((posts) => {
-      console.log('metallllll')
-      res.render('posts/result', { posts: location })
-    }).catch((error) => {
-      console.log(error)
-      res.send('RESULTS NOT RENDERING')
-    })
+  console.log('result route')
+  console.log(req.query)
+  db.post.findAll({
+    where: { location: req.query.location }
+  }).then((posts) => {
+    console.log('metallllll')
+    res.render('posts/result', { posts: location })
+  }).catch((error) => {
+    console.log(error)
+    res.send('RESULTS NOT RENDERING')
   })
+})
 
 // Form to Create Post
 router.get("/new", (req, res) => {
-    db.post.findAll()
+  db.post.findAll()
     .then((post) => {
-        res.render("posts/new", { post: post })
+      res.render("posts/new", { post: post })
     })
     .catch((error) => {
-        res.status(200).send("new posts")
+      res.status(200).send("new posts")
     })
 })
 
 // Post to Profile
 router.post("/new", (req, res) => {
-    db.post.create({
-        city: req.body.city,
-        country: req.body.country,
-        content: req.body.content,
-        title: req.body.title
-    })
+  db.post.create({
+    city: req.body.city,
+    country: req.body.country,
+    content: req.body.content,
+    title: req.body.title
+  })
     .then((post) => {
-        res.render("posts/image", {post: post})
+      res.render("posts/image", { post: post })
     })
     .catch((error) => {
-        console.log(error)
+      console.log(error)
     })
 })
 
 // Profile -- ALL POSTS
-router.get("/", (req, res)=>{
+router.get("/", (req, res) => {
   db.post.findAll()
-  .then((posts)=>{
-    res.render("posts/index", { posts })
-  })
-  .catch(err => console.log(err))
+    .then((posts) => {
+      res.render("posts/index", { posts })
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
